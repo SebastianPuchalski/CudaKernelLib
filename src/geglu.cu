081d3c769c412da878d7210dc4772141f2786eba
Sebastian <seb@fakeemail>
2024-06-23 18:18:50 +0200

    Improvements in geglu

--- src/geglu.cu
+++ src/geglu.cu
@@ -3,42 +3,44 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
+#include <sstream>
+#include <iomanip>
+
 __device__ __inline__ float gelu(float x) {
 	const float A = 0.7978845608f; // sqrt(2 / PI)
 	const float C = 0.044715f;
 	return 0.5f * x * (1.f + tanh(A * (x + C * x * x * x)));
 }
 
-__global__ void gegluKernel(float* out, const float* in, int batchSize, int dim)
+__global__ void gegluKernel(float* out, const float* in, int outSize, int halfDim)
 {
 	int outIdx = threadIdx.x + blockDim.x * blockIdx.x;
-	int halfDim = dim / 2;
-	if (outIdx < batchSize * halfDim) {
-		int d = outIdx % halfDim;
-		int b = outIdx / halfDim;
-		int idx1 = b * dim + d;
+	if (outIdx < outSize) {
+		int idx1 = outIdx * 2 - outIdx % halfDim;
 		int idx2 = idx1 + halfDim;
 		out[outIdx] = gelu(in[idx1]) * in[idx2];
 	}
 }
 
 void geglu(float* out, const float* in, int batchSize, int dim) {
-	const int THREAD_PER_BLOCK = 256;
-	int numberOfBlocks = ((batchSize * dim / 2) + THREAD_PER_BLOCK - 1) / THREAD_PER_BLOCK;
-	gegluKernel<<<numberOfBlocks, THREAD_PER_BLOCK >>>(out, in, batchSize, dim);
+	assert(dim % 2 == 0);
+	int halfDim = dim / 2;
+	int outSize = batchSize * halfDim;
+	const int BLOCK_SIZE = 256;
+	int gridSize = (outSize + BLOCK_SIZE - 1) / BLOCK_SIZE;
+	gegluKernel<<<gridSize, BLOCK_SIZE>>>(out, in, outSize, halfDim);
 }
 
-using KernelFunction = void(*)(float*, const float*, int, int);
-
-float geglu(CudaBuffer<float>& outTensor, const CudaBuffer<float>& inTensor,
-	int batchSize, int dim, KernelFunction kernelFunc) {
+float geglu(CudaBuffer<float>& outTensor,
+			const CudaBuffer<float>& inTensor,
+			int batchSize, int dim) {
 	CudaBuffer<float> outTensorDev(outTensor.size(), cudaMemoryTypeDevice);
 	CudaBuffer<float> inTensorDev(inTensor.size(), cudaMemoryTypeDevice);
 	inTensorDev.copyFrom(inTensor);
 
 	CudaEvent start, stop;
 	start.record();
-	kernelFunc(outTensorDev(), inTensorDev(), batchSize, dim);
+	geglu(outTensorDev(), inTensorDev(), batchSize, dim);
 	checkCudaError(cudaGetLastError());
 	stop.record();
 	stop.synchronize();
@@ -55,6 +57,7 @@ float geluRef(float x) {
 }
 
 void gegluRef(float* out, const float* in, int batchSize, int dim) {
+	assert(dim % 2 == 0);
 	for (int b = 0; b < batchSize; b++) {
 		for (int d = 0; d < dim / 2; d++) {
 			int idx1 = b * dim + d;
@@ -64,7 +67,7 @@ void gegluRef(float* out, const float* in, int batchSize, int dim) {
 	}
 }
 
-void testGeGLU(int batchSize, int dim, KernelFunction kernelFunc) {
+void testGeGLU(int batchSize, int dim) {
 	assert(batchSize > 0);
 	assert(dim > 0 && dim % 2 == 0);
 
@@ -73,23 +76,30 @@ void testGeGLU(int batchSize, int dim, KernelFunction kernelFunc) {
 
 	const int outTensorSize = inTensor.size() / 2;
 	CudaBuffer<float> outTensor(outTensorSize, cudaMemoryTypeHost);
-	float time = geglu(outTensor, inTensor, batchSize, dim, kernelFunc);
+	float time = geglu(outTensor, inTensor, batchSize, dim);
 	CudaBuffer<float> outTensorRef(outTensorSize, cudaMemoryTypeHost);
 	gegluRef(outTensorRef(), inTensor(), batchSize, dim);
 
 	bool pass = outTensor.approxEqual(outTensorRef);
-	std::string name = "GeGLU(";
-	name += std::to_string(batchSize) + "x";
+	std::string name = "GeGLU (";
+	name += std::to_string(batchSize) + ", ";
 	name += std::to_string(dim) += ")";
-	printTestItem(name, pass, time);
+
+	float theoreticalTime = getBestMemoryAccessTime(outTensorSize * sizeof(float));
+	theoreticalTime *= 3 * 1000; // read input and write output in milliseconds
+	float efficiency = theoreticalTime / time;
+	std::ostringstream addInfo;
+	addInfo << std::fixed << std::setprecision(0);
+	addInfo << efficiency * 100 << "%";
+
+	printTestItem(name, pass, time, addInfo.str());
+	checkCudaError(cudaDeviceReset());
 }
 
 void testGeGLU() {
 	checkCudaError(cudaSetDevice(0));
 
-	testGeGLU(64, 4096, geglu);
-	testGeGLU(256, 4096, geglu);
-
-	checkCudaError(cudaGetLastError());
-	checkCudaError(cudaDeviceReset());
+	testGeGLU(64, 1024 * 128);
+	testGeGLU(256, 1024 * 128);
+	testGeGLU(1024, 1024 * 128);
 }
